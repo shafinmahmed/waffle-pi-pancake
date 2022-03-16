cfb2fa465fb7acbce2802a82357c80368c3be6f1
Shafin Ahmed <shafin.m.ahmed@gmail.com>
2022-03-15 23:41:03 -0400

    feat: locate_marker script and launch set for waffle

--- src/locate_marker.py
+++ src/locate_marker.py
@@ -1,30 +1,74 @@
 #!/usr/bin/env python
 
 import rospy
-import math
 import tf
-from geometry_msgs.msg import Pose
+from visualization_msgs.msg import Marker
 
-def tf_listener_test():
-    rospy.init_node('tf_listener_test', anonymous=True)
+flag = False
+
+def create_marker(trans, rot):
+    # publisher = rospy.Publisher('visualization_marker/ArUco_Location', Marker, queue_size = 50)
+
+    marker = Marker()
+
+    marker.header.frame_id = "/map"
+    marker.type = marker.SPHERE
+    marker.action = marker.ADD
+    marker.header.stamp = rospy.Time.now()
+
+    # set shape, Arrow: 0; Cube: 1 ; Sphere: 2 ; Cylinder: 3
+    marker.type = 2
+    marker.id = 0
+
+    # Set the scale of the marker
+    marker.scale.x = 1.0
+    marker.scale.y = 1.0
+    marker.scale.z = 1.0
+
+    # Set the color
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 0.0
+    marker.color.a = 1.0
+
+    # Set the pose of the marker
+    marker.pose.position.x = trans[0]
+    marker.pose.position.y = trans[1]
+    marker.pose.position.z = trans[2]
+    marker.pose.orientation.x = rot[0]
+    marker.pose.orientation.y = rot[1]
+    marker.pose.orientation.z = rot[2]
+    marker.pose.orientation.w = 1.0
+
+    return marker
+
+    # publisher.publish(marker)
+
+def locate_marker():
+    rospy.init_node('locate_marker', anonymous=True)
     listener = tf.TransformListener()
+
 
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = listener.lookupTransform('/base_link', '/fiducial_2', rospy.Time(0))
+            (trans,rot) = listener.lookupTransform('/fiducial_2', '/map', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
         
-        rospy.loginfo('trans: ' + str(trans[0]) + ' ' + str(trans[1]) + ' ' + str(trans[2]))
+        publisher = rospy.Publisher('visulization_marker/ArUco_Location', Marker, queue_size=50)
+
+        marker = create_marker(trans, rot)
+
+        publisher.publish(marker)
 
 
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        tf_listener_test()
+        locate_marker()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
